3c0ce42461357d4129fbc638c5e4ab5a2eee777f
Yohan Jarosz <yohan.jarosz@epfl.ch>
2010-12-21 16:21:04 +0100

    adding missing files

---
+++ src/main/java/ch/epfl/bbcf/gdv/html/AddUserToGroupPage.java
@@ -0,0 +1,49 @@
+package ch.epfl.bbcf.gdv.html;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.value.ValueMap;
+
+import ch.epfl.bbcf.gdv.config.UserSession;
+import ch.epfl.bbcf.gdv.control.model.GroupControl;
+import ch.epfl.bbcf.gdv.html.GroupPage.CustModalWindow;
+import ch.epfl.bbcf.gdv.html.utility.FormChecker;
+
+public class AddUserToGroupPage extends WebPage{
+
+	private FeedbackPanel feedback;
+
+	protected final ValueMap properties = new ValueMap();
+
+	public AddUserToGroupPage(GroupPage groupPage, final CustModalWindow modal) {
+		final Form form = new Form("form");
+		form.add(new AjaxButton("submit"){
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form_) {
+				String userMail = properties.getString("user_mail");
+				FormChecker checker = new FormChecker(form, (UserSession)getSession());
+				checker.checkMail(userMail);
+				if(checker.isFormSubmitable()){
+					GroupControl gc = new GroupControl((UserSession)getSession());
+					gc.addUserToGroup(modal.getGroupId(),userMail);
+					modal.close(target);
+				}
+
+			}
+		});
+
+		TextField text = new TextField("user_mail",new PropertyModel(properties,"user_mail"));
+		form.add(text);
+
+		feedback = new FeedbackPanel("feedback");
+		form.add(feedback);
+		add(form);
+	}
+
+
+}
---
+++ src/main/java/ch/epfl/bbcf/gdv/html/wrapper/ProjectWrapper.java
@@ -0,0 +1,95 @@
+package ch.epfl.bbcf.gdv.html.wrapper;
+
+import java.io.Serializable;
+import java.util.List;
+
+import ch.epfl.bbcf.gdv.access.database.pojo.Project;
+import ch.epfl.bbcf.gdv.access.database.pojo.Sequence;
+import ch.epfl.bbcf.gdv.html.utility.SelectOption;
+
+public class ProjectWrapper implements Serializable{
+	
+	private Project project;
+	private String speciesName;
+	private int speciesId;
+	private List<SelectOption> sequences;
+	private int tracksNumber;
+	private List<TrackWrapper> tracks;
+	
+	
+	public ProjectWrapper(Project p){
+		this.project = p;
+	}
+
+	public int getId(){
+		return this.project.getId();
+	}
+	public int getSequenceId(){
+		return this.project.getSequenceId();
+	}
+	public void setSequenceId(String seqId){
+		this.project.setCurrentSequenceId(Integer.parseInt(seqId));
+	}
+	public String getDescription(){
+		return this.project.getName();
+	}
+
+	/**
+	 * @param species the species to set
+	 */
+	public void setSpeciesId(int speciesId) {
+		this.speciesId = speciesId;
+	}
+
+	/**
+	 * @return the species
+	 */
+	public int getSpeciesId() {
+		return speciesId;
+	}
+
+	/**
+	 * @param tracksNumber the tracksNumber to set
+	 */
+	public void setTracksNumber(int tracksNumber) {
+		this.tracksNumber = tracksNumber;
+	}
+
+	/**
+	 * @return the tracksNumber
+	 */
+	public int getTracksNumber() {
+		return tracksNumber;
+	}
+
+	/**
+	 * @param tracks the tracks to set
+	 */
+	public void setTracks(List<TrackWrapper> tracks) {
+		this.tracks = tracks;
+	}
+
+	/**
+	 * @return the tracks
+	 */
+	public List<TrackWrapper> getTracks() {
+		return tracks;
+	}
+
+	public void setSpeciesName(String speciesName) {
+		this.speciesName = speciesName;
+	}
+
+	public String getSpeciesName() {
+		return speciesName;
+	}
+
+	public void setSequences(List<SelectOption> sequences) {
+		this.sequences = sequences;
+	}
+
+	public List<SelectOption> getSequences() {
+		return sequences;
+	}
+
+}
