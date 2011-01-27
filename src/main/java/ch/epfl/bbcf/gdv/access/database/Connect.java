1e96ca5bad3676ec89a7067d32b71d407f58cc5f
Yohan Jarosz <yohan.jarosz@epfl.ch>
2011-01-27 16:42:26 +0100

    PROD MIGRATION

--- src/main/java/ch/epfl/bbcf/gdv/access/database/Connect.java
+++ src/main/java/ch/epfl/bbcf/gdv/access/database/Connect.java
@@ -41,7 +41,7 @@ import ch.epfl.bbcf.gdv.config.UserSession;
 public class Connect implements Connection{
 
 	public static final String DRIVER = "org.postgresql.Driver";
-	public static final String BASE = "gdv_dev";
+	public static final String BASE = "gdv_prod_1.2";//"gdv_dev";
 	public static final String	URL = "jdbc:postgresql://127.0.0.1/"+BASE;
 	public static final String USER = "java";
 	public static final String PASSWD ="java_gdv_bbcf";
