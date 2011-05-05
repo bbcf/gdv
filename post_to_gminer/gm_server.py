114ccb0f4d740c1d4919adbc9b9d7be886fabe4c
Lucas Sinclair <lucas.sinclair@me.com>
2011-05-05 16:26:52 +0200

    In the process of rewriting the GM server

--- post_to_gminer/gm_server.py
+++ post_to_gminer/gm_server.py
@@ -1,24 +1,43 @@
-# General Modules #
-import cherrypy, httplib2, urllib
+"""
+=================
+Script: gm_server
+=================
 
-# gFeatMiner #
-from gMiner import gmJob
-from gMiner.gm_constants import *
+Implementation a web server around the functinoality of gFeatMiner. 
+
+The servers listens for requests. Formats them. Sends them off to the gMiner library. Formats the result obtained and sends that off in a HTTP POST to an URL specified in the original request. This can be usefull if GDV wants to send requests to an instance of gm_server and recieve answers.
+
+A typical request arriving to the server would be:
+
+{'form':'''{"compare_parents":["compare_parents"],"per_chromosome":[],"selected_regions":"M:7455 .. 7720;M:8315 .. 8792","tracks":"{1:{'name':'Rip genes','path':'/tpm/asddasd.sql'},2:{'name':'rp genes','path':'/tmp/adsd.sql}}"}''','callback_url':'http://svitsrv25.epfl.ch/gdv_dev/GFeatMiner','from':'1','job_id':'1'}
+
+A typical answer from the server would be:
+
+{'from':1,'job_id':'1','results':{'files':['/tmp/asd.sql','/tmp/qq.sql'],'type':'sql'}}
+
+"""
+
+# General modules #
+import cherrypy, httplib2, urllib, json
+
+# Other modules #
+import gMiner
+
+# Specific variables #
+from gMiner.gm_constants import gm_project_name, gm_project_version
 
 ###########################################################################
 class gmServer(object):
     def __init__(self, port=7522):
-        self.HTTP_PORT = port
-        cherrypy.log("init of the server")
+        self.port = port
          
     def serve(self):
-        cherrypy.log("serve")
         # Change the server name #
         serverTag = gm_project_name + "/" + str(gm_project_version)
         cherrypy.config.update({'tools.response_headers.on':      True,
                                 'tools.response_headers.headers': [('Server', serverTag)]})
         # Change the port #
-        cherrypy.server.socket_port = self.HTTP_PORT
+        cherrypy.server.socket_port = self.port
         # Add post processing #
         cherrypy.tools.post_process = cherrypy.Tool('on_end_request', post_process)
         # Start Server #
@@ -26,113 +45,38 @@ class gmServer(object):
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
              'tools.post_process.on': True}})
 
-#-------------------------------------------------------------------------#
 class CherryRoot(object):
     exposed = True
     def GET(self, **kwargs):
-        return self.POST(**kwargs)
-
+        return pre_process(**kwargs)
     def POST(self, **kwargs):
-        nargs = format_request(**kwargs)
-        # Run the request #
-        global job
-        job = gmJob(nargs)
-        error, result, type = job.prepare_catch_errors()
-        # Check if callback or errors #
-        if error != 200:
-            del job
-        else:
-            if not job.request.has_key('callback_url'):
-                error, result, type = job.run_catch_errors()
-                del job
-        # Return result #
-        cherrypy.response.headers['Content-Type'] = type
-        cherrypy.response.status = error
-        return result
+        return pre_process(**kwargs)
 
-    # def GET(self):
-    #     cherrypy.response.status = 301
-    #     cherrypy.response.headers['Location'] = gm_doc_url
-    #     return 'Redirecting to documentation'
+def pre_process(**kwargs):
+    # Create a job # 
+    global job
+    job = kwargs
+    return 'Job added to queue'
 
 #-------------------------------------------------------------------------#
 def post_process(**kwargs):
-    # Check for a job #
-    cherrypy.log("post process :"+str(kwargs))
+    # Format the request #
     global job
+    request = {}
+    # Run the request #
     try:
-        if not job:
-            cherrypy.log("no job")
-            return
-    except NameError:
-        return
-    if not job.request.has_key('callback_url'): return
-    # Run the job #
-    error, result, type = job.run_catch_errors()
-    # Get the id #
-    if job.request.has_key('id'):
-        id = job.request['id']
-        body = urllib.urlencode({'id':     id,
-                                 'status': error, 
-                                 'result': str(result),
-                                 'type':   type})
+        result = gMiner.run(request)
+    except Exception as err:
+        answer = str(err)
     else:
-        body = urllib.urlencode({'status': error, 
-                                 'result': str(result),
-                                 'type':   type})
+        type = result[0].split('.')[-1]
+        answer = {'files': result, 'type': type}
     # Make an HTTP POST #
     connection = httplib2.Http()
+    body = urllib.urlencode({'from': job['from'],'job_id': job['job_id'], 'result': answer})
     headers = {'content-type': 'application/x-www-form-urlencoded'}
-    address = job.request['callback_url']
-    # Send it #
-    response, content = connection.request(address, "POST",
-                                           body   = body,
-                                           headers= headers)
+    address = job['callback_url']
+    response, content = connection.request(address, "POST", body=body, headers=headers)
 
 ###########################################################################
-
-
-TEST_REQUEST = {'form':"""{"compare_parents":["compare_parents"],"per_chromosome":["per_chromosome"],"selected_regions":"M:7455 .. 7720;M:8315 .. 8792","tracks":"Rip140_day0_May_treat_afterfiting_chr6.wig;Rip140_day0_May_treat_afterfiting_chr7.wig"}""",'callback_url':'http://svitsrv25.epfl.ch/gdv_dev/GFeatMiner','from':'1','job_id':'1','version':'1.0.0'}
-import json
-
-"""
-
-Format the request to process a GFM job
-
-"""
-
-def format_request(**kwargs):
-    nargs={}
-    kwargs=TEST_REQUEST
-    cherrypy.log("format request :"+str(kwargs))
-    params="";
-    if(kwargs['form']):
-        params=json.loads(kwargs['form'])
-        for param in params :
-            if(param=="selected_regions"):
-                handle_selected_regions(nargs,params[param],param)
-            elif(param=="tracks"):
-                handle_tracks(nargs,params[param])
-            else :
-                nargs[param]=params[param]
-
-    ##TODO put versionning
-    ## and the other parameters
-    cherrypy.log(str(nargs))
-    return nargs
-
-
-
-
-def handle_selected_regions(dic,params,param):
-    dic[param]=params
-def handle_tracks(dic,tracks):
-    l=tracks.split(";")
-    cherrypy.log(str(len(l)))
-    for i in range(1,len(l)+1):
-        dic["track_"+str(i)]=l[i-1]
-    
-
-
-
 if __name__ == '__main__': gmServer().serve()
