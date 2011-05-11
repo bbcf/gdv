edc1d07f75893feb1d37eddb6612f44b6b84e19a
Yohan Jarosz <yohanjarosz@yahoo.fr>
2011-05-11 14:12:09 +0200

    adding the script that will build the archive neede for installation

---
+++ build_scripts/update_archive.sh
@@ -0,0 +1,92 @@
+#/bin/sh
+
+CUR=$PWD
+
+
+if [ -z $1 ]; then
+    echo 'GDV_HOME path is missing : provide the full path to the HOME dir of GDV project'
+    exit 1
+fi
+GDV_HOME=$1
+
+
+
+if [ -z $2 ];then 
+    echo "must provide a version number"
+    exit 1
+fi
+VERSION=$2
+
+
+
+cd $GDV_HOME
+TMP_DIR="tmp_build"
+
+
+mkdir $TMP_DIR
+mkdir $TMP_DIR/$VERSION
+
+echo "#############################################################################################################"
+echo "######################                          BUILD TREE                        ###########################"
+echo "#############################################################################################################"
+
+#build tree
+sh $GDV_HOME/build_scripts/build_tree.sh $GDV_HOME $GDV_HOME/$TMP_DIR/$VERSION
+
+echo "#############################################################################################################"
+echo "######################                          BUILD WAR                         ###########################"
+echo "#############################################################################################################"
+
+#build war archive & daemons 
+mkdir $TMP_DIR/$VERSION/gdv/bin
+
+#archive project
+cd $GDV_HOME
+mvn clean package
+mv target/gdv-$VERSION.war target/gdv.war
+chmod 777 target/gdv.war
+cp target/gdv.war $TMP_DIR/$VERSION/gdv/bin/.
+
+cp build_scripts/start_gdv.sh $TMP_DIR/$VERSION/gdv/bin/.
+cp build_scripts/stop_gdv.sh $TMP_DIR/$VERSION/gdv/bin/.
+
+echo "#############################################################################################################"
+echo "######################                       BUILD DAEMON  1                      ###########################"
+echo "#############################################################################################################"
+
+#archive daemons
+cd conversion/compute_sqlite_scores
+ant jar
+
+echo $PWD
+ls
+
+cp compute_to_sqlite.jar $GDV_HOME/$TMP_DIR/$VERSION/gdv/compute_sqlite_scores/.
+cp start.sh $GDV_HOME/$TMP_DIR/$VERSION/gdv/compute_sqlite_scores/.
+cp stop.sh $GDV_HOME/$TMP_DIR/$VERSION/gdv/compute_sqlite_scores/.
+
+echo "#############################################################################################################"
+echo "######################                        BUILD DAEMON  2                     ###########################"
+echo "#############################################################################################################"
+
+cd $GDV_HOME/conversion/transform_to_sqlite
+ant jar
+cp transform_to_sqlite.jar $GDV_HOME/$TMP_DIR/$VERSION/gdv/transform_to_sqlite/.
+cp start.sh $GDV_HOME/$TMP_DIR/$VERSION/gdv/transform_to_sqlite/.
+cp stop.sh $GDV_HOME/$TMP_DIR/$VERSION/gdv/transform_to_sqlite/.
+
+echo "#############################################################################################################"
+echo "######################                        ARCHIVE TREE                        ###########################"
+echo "#############################################################################################################"
+
+#archive tree
+cd $GDV_HOME
+tar -czf $TMP_DIR/$VERSION/gdv.tgz $TMP_DIR/$VERSION/gdv
+rm -r $GDV_HOME/$VERSION/gdv
+echo "done ... "
+
+
+
+
+cd $CUR
+exit 0
