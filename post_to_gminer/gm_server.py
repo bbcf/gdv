122bf19cf38347b7ec41ef46526f9fa6afeb73c0
Yohan Jarosz <yohanjarosz@yahoo.fr>
2011-06-15 10:06:34 +0200

    modify style and gfeatminer request & response spec

--- post_to_gminer/gm_server.py
+++ post_to_gminer/gm_server.py
@@ -7,17 +7,54 @@ Implementation of a web server around the functionality of gFeatMiner.
 
 The servers listens for requests. When it gets one, it formats them. Then, sends the request off to the gMiner library. Formats the result obtained and sends that off in a HTTP POST to an URL specified in the original request. This can be useful if GDV wants to send requests to an instance of gm_server and receive answers.
 
-A typical request arriving to the server would be:
+################################################################
+#########################   REQUEST   ##########################
+################################################################
+A typical request arriving to the server would be (HTML FORM ENCODED):
+ - output_location = /tmp/ (path where you want the output)
+ - callback_url = http://server.org/gfeatminer (URL to post back the response)
+ - data= {} (the data from the web form, it is JSON formatted)
 
-{'form':'''{"operation_type":"desc_stat","characteristic":"number_of_features","compare_parents":[],"per_chromosome":["per_chromosome"],"selected_regions":"chr1:0 .. 10000000;chr2:0 .. 10000000","tracks":{"1":{"name":"S. cer refseq genes","path":"/scratch/genomic/tracks/all_yeast_genes.sql"},"2":{"name":"RP genes","path":"/scratch/genomic/tracks/ribosome_proteins.sql"}},"output_location":"/tmp/"}''','callback_url':'http://svitsrv25.epfl.ch/gdv_dev/GFeatMiner','from':'1','job_id':'1'}
+ # detailled description of 'data' parameter :
 
-A typical answer from the server would be:
+ - operation_type : 'desc_stat' (self-explanatory)
+ - characteristic : 'base_coverage' (self-explanatory)
+ - compare_parents :[] (a boolean)
+ - per_chromosome : ['per_chromosome'] (idem)
+ - filter:[{path:'/data/gdv_dev/files/115d5da7db0c588ae95bb91a5710ba2147be3df0.db',name:'foo'}] (path to a file that take the role of a filter, a selection)
+ - ntracks:[{path:'/data/gdv_dev/files/d08aa4569c17aa79abc57c3b44da6abab927fa2.db',name:'bar'},{...},...] (path to the file(s) to process. It can be ordered)
+ - job_id : 1 (the identifier of the job)
 
-{'from':1,'job_id':'1','result':'''{"files":[{"path":"/tmp/asd.sql","type":"sql"},{"path":"/tmp/qq.png","type":"png"}]}'''}
+
+
+E.G. : 
+{'output_location': u'/data/gdv_dev/gFeatMiner/208', 'data': u'{"compare_parents":["compare_parents"],"per_chromosome":["per_chromosome"],"characteristic":"base_coverage","operation_type":"desc_stat","ntracks":[{"name":"RNAPol_fwd.sql","path":"/data/gdv_dev/files/6cb8a2f895e2cf829912bdbd244a6092289fe82f.db"}],"filter":[{"name":"chr(2839500,2909000)_chr(3023000,3070000)_.db","path":"/data/gdv_dev/files/952f788e0e0c5b43ec854515c9ebbad6a5c5e771.db"}]}', 'callback_url': u'http://svitsrv25.epfl.ch/gdv_dev/post', 'output_name': u'gfeatminer_output', 'job_id': u'208'}
+
+
+
+################################################################
+#########################  RESPONSE  ###########################
+################################################################
+
+A typical answer from the server would be (HTML FORM ENCODED):
+ 
+ - id = 'job' (static)
+ - action = 'gfeatresponse' (static)
+ - job_id = the job identifier
+ - data = {} (the result, JSON formatted)
+ 
+# detailled description of 'data' parameter :
+ - files : [] a list of result files in JSON
+ -  ...  path : path to the file
+ -  ...  type : type of the file
+ 
+E.G. :
+
+{'id':'job','action':'gfeatresponse','job_id':'1','result':'''{"files":[{"path":"/tmp/asd.sql","type":"sql"},{"path":"/tmp/qq.png","type":"png"}]}'''}
 
 If an error is found, the answer could be:
 
-{'from':1,'job_id':'1','result':'''{"type":"error","msg":"Text about error","html":"<html>Displayable version for user</html>"'''}
+{'id':'job','action':'gfeatresponse','job_id':'1','result':'''{"type":"error","msg":"Text about error","html":"<html>Displayable version for user</html>"'''}
 """
 
 # General modules #
@@ -68,16 +105,20 @@ def pre_process(**kwargs):
 def post_process(**kwargs):
     global jobs
     job = jobs.pop(0)
+    print "#######################"
+    print job
+    print '#######################'
     try:
         # Format the input #
-        request = json.loads(job['form'])
-        if request.has_key('compare_parents' ): request['compare_parents' ] = bool         (request['compare_parents' ])
-        if request.has_key('per_chromosome'  ): request['per_chromosome'  ] = bool         (request['per_chromosome'  ])
-        if request.has_key('selected_regions'): request['selected_regions'] = parse_regions(request['selected_regions'])
+        request = json.loads(job['data'])
+        if request.has_key('compare_parents' ): request['compare_parents' ] = bool(request['compare_parents' ])
+        if request.has_key('per_chromosome'  ): request['per_chromosome'  ] = bool(request['per_chromosome'  ])
+        # Filter #
+        
         # Special parameters #
-        if request.has_key('tracks'):
-            request.update(parse_tracks(request['tracks']))
-            request.pop('tracks')
+        # if request.has_key('tracks'):
+        #     request.update(parse_tracks(request['tracks']))
+        #     request.pop('tracks')
         # Extra parameters #
         request['output_location'] = job['output_location']
         # Unicode filtering #
